13e9a6eb69c6cc22e99d3abe78235e0130f5cd21
mivantec <91033130+mivantec@users.noreply.github.com>
2021-11-25 11:42:43 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='advanced-bandit-problems',
+      version='0.0.1',
+      description="Non stationary bandits.",
+      url='',
+      author='Guilherme Duarte Marmerola',
+      author_email='gdmarmerola@gmail.com',
+      license='Apache 2.0',
+      packages=['advanced-bandit-problems'],
+      zip_safe=False)
